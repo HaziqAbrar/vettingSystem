e2a1fdcae51625d150ad69ad8779f31478bb7aea
renomus <ammarashraff98@gmail.com>
2020-12-23 12:49:39 +0800

    student

--- resources/views/student/profile.blade.php
+++ resources/views/student/profile.blade.php
@@ -1,5 +1,5 @@
 <x-sidebarStudent>
-
+<div class="container-table100">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
@@ -70,5 +70,5 @@
     </div>
 
 
-
+</div>
 </x-sidebarStudent>
