d2cb4b0c335c09640e3afced4de62b7e4c52b177
Haziq Abrar <haziqabrar@gmail.com>
2021-01-15 20:05:07 +0800

    image resources

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-  <img src="{{ asset('images/try.jpg') }}" class="w-16 h-16" viewbox="0 0 48 48">
+  <img src="{{ asset('images/try.png') }}" class="w-16 h-16" viewbox="0 0 48 48">
 </a>
